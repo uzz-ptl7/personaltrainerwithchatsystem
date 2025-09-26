b5a85423da27dc2b9a6249e7ffb1bcdd88cc71ab
uzz-ptl7 <uzairpatel7117@gmail.com>
2025-09-27 00:06:16 +0200

    Fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,12 +6,13 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAQKLC8CdOobgZsDfNxe2SHuL-WmZHGl4M",
-  authDomain: "ptcsystem-c7a07.firebaseapp.com",
-  projectId: "ptcsystem-c7a07",
-  storageBucket: "ptcsystem-c7a07.appspot.com",
-  messagingSenderId: "184544207619",
-  appId: "1:184544207619:web:80b74e5067f3158abcff36",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 });
 
 const messaging = firebase.messaging();
